478f84992eadcec2206c54e14cd6e787059ede93
PC-73 <huangfengrui>
2020-01-15 17:59:57 +0800

    隐藏面包屑

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,20 @@
+<!--
+ * @LastEditors  : huangfengrui
+ * @LastEditTime : 2020-01-15 17:48:14
+ * @Author: huangfengrui
+ * @Date: 2020-01-09 11:06:30
+ * @Description:
+ -->
 <template lang="pug">
 el-container
-  Headerbar(
-    :headerMenuList="headerMenuList")
+  Headerbar
   Siderbar
   .content-box(
     :class="{'content-collapse':collapse}")
-    el-breadcrumb.crumbs(separator="/")
-      el-breadcrumb-item(:to="{ path: '/' }") 首页
-      el-breadcrumb-item 表单
+    //- el-breadcrumb.crumbs(separator="/")
+    //-   el-breadcrumb-item(
+    //-     v-for="breadcrumb in breadcrumbList"
+    //-     :to="breadcrumb.path") {{breadcrumb.name}}
     .content
       .container
         transition(name="fade-transform" mode="out-in")
@@ -19,6 +26,8 @@ el-container
 import { Component, Vue, Provide } from 'vue-property-decorator'
 import Headerbar from '@/components/Headerbar.vue'
 import Siderbar from '@/components/Siderbar.vue'
+import { cloneDeep } from '@/utils/common'
+import menus from '@/utils/menu'
 
 @Component({
   components: {
@@ -29,61 +38,47 @@ import Siderbar from '@/components/Siderbar.vue'
 export default class Home extends Vue {
   @Provide() collapse: boolean = false
   @Provide() tagsList: any = []
-  @Provide() headerMenuList: object[] = [{
-    label: '地图管理',
-    name: 'mapManage',
-    path: '/mapManage',
-    icon: 's-cooperation'
-  }, {
-    label: '数据管理',
-    name: 'dataManage',
-    path: '/dataManage',
-    icon: 's-cooperation'
-  }, {
-    label: '产业监测',
-    name: 'industryMonitor',
-    path: '/industryMonitor',
-    icon: 's-cooperation'
-  }, {
-    label: '应急指挥',
-    name: 'command',
-    path: '/command',
-    icon: 's-cooperation'
-  }, {
-    label: '信息发布',
-    name: 'information',
-    path: '/information',
-    icon: 's-cooperation'
-  }, {
-    label: '产业管理',
-    name: 'industryManage',
-    path: '/industryManage',
-    icon: 's-cooperation'
-  }, {
-    label: '决策分析',
-    name: 'decision',
-    path: '/decision',
-    icon: 's-cooperation'
-  }, {
-    label: '应用支撑',
-    name: 'application',
-    path: '/application',
-    icon: 's-cooperation'
-  }, {
-    label: '平台管理',
-    name: 'platform',
-    path: '/platform',
-    icon: 's-cooperation'
-  }, {
-    label: '监控管理',
-    name: 'monitor',
-    path: '/monitor',
-    icon: 's-cooperation'
-  }]
+  @Provide() menuList: object[] = menus
+
   created () {
     this.$bus.$on('collapse', (msg: boolean) => {
       this.collapse = msg
     })
+  }
+  get breadcrumbList () {
+    console.log('this.menuList', this.menuList)
+    const routeMatched = this.$route.matched
+      .filter((item, i) => i >= 1)
+      .map((item:any) => {
+        const currentMenu:any = this.menuList.find((menuItem:any) => item.path === menuItem.url)
+        console.log('currentMenu', currentMenu)
+        if (currentMenu && currentMenu.submenu) {
+          const subMenu = currentMenu.submenu.find((menuItem:any) => {
+            console.log('menuItem.url', menuItem.url)
+            console.log('item.path', item)
+            return menuItem.url === item.path
+          })
+          console.log(subMenu)
+        //   return {
+        //     name: subMenu.label,
+        //     path: currentMenu.path
+        //   }
+        }
+        // const currentMenu:any = this.menuList[index]
+        // if (currentMenu.submenu) {
+        //   const subIndex = currentMenu.submenu.findIndex((item:any) => currentRoute[2].path === item.url)
+        //   return `${index}-${subIndex}`
+        // } else {
+        //   return `1-${index}`
+        // }
+        return {
+          name: 'xxx',
+          path: item.path
+        }
+      })
+    // const breadcrumbList = routeMatched.slice(1, 2)
+    console.log(routeMatched)
+    return routeMatched
   }
 }
 </script>
